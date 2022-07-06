6f466bf322f5e899a83921b98b64acf15e6a5045
Maxwell West <westm2@student.unimelb.edu.au>
2022-07-06 13:07:06 +1000

    code so far

---
+++ main.py
@@ -0,0 +1,201 @@
+import qiskit
+from qiskit import Aer, QuantumCircuit
+from qiskit.circuit import Parameter
+import numpy as np
+import torch
+import torchvision
+from torch import cat, no_grad, manual_seed
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import torch.optim as optim
+from torch.nn import (
+    Module,
+    Conv2d,
+    Linear,
+    Dropout2d,
+    NLLLoss,
+    MaxPool2d,
+    Flatten,
+    Sequential,
+    ReLU,
+)
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from qiskit.circuit.library import RealAmplitudes, ZZFeatureMap
+from qiskit_machine_learning.neural_networks import CircuitQNN, TwoLayerQNN
+from qiskit_machine_learning.connectors import TorchConnector
+from qiskit.opflow import AerPauliExpectation
+from qiskit.utils import QuantumInstance, algorithm_globals
+
+transform = transforms.Compose([
+    transforms.CenterCrop(18),
+    transforms.Resize(7),
+    transforms.ToTensor()])
+# Train Dataset
+# -------------
+
+# Set train shuffle seed (for reproducibility)
+manual_seed(42)
+
+batch_size = 1
+n_samples = 100  # We will concentrate on the first 100 samples
+
+# Use pre-defined torchvision function to load MNIST train data
+X_train = datasets.MNIST(
+    root="./data", train=True, download=True, transform=transform
+)
+
+# Filter out labels (originally 0-9), leaving only labels 0 and 1
+idx = np.append(
+    np.where(X_train.targets == 0)[0][:n_samples], np.where(X_train.targets == 1)[0][:n_samples]
+)
+X_train.data = X_train.data[idx]
+X_train.targets = X_train.targets[idx]
+
+# Define torch dataloader with filtered data
+train_loader = DataLoader(X_train, batch_size=batch_size, shuffle=True)
+
+n_samples_show = 6
+
+data_iter = iter(train_loader)
+fig, axes = plt.subplots(nrows=1, ncols=n_samples_show, figsize=(10, 3))
+
+while n_samples_show > 0:
+    images, targets = data_iter.__next__()
+
+    axes[n_samples_show - 1].imshow(images[0, 0].numpy().squeeze(), cmap="gray")
+    axes[n_samples_show - 1].set_xticks([])
+    axes[n_samples_show - 1].set_yticks([])
+    axes[n_samples_show - 1].set_title("Labeled: {}".format(targets[0].item()))
+
+    n_samples_show -= 1
+
+# Test Dataset
+# -------------
+
+# Set test shuffle seed (for reproducibility)
+# manual_seed(5)
+
+n_samples = 50
+
+# Use pre-defined torchvision function to load MNIST test data
+X_test = datasets.MNIST(
+    root="./data", train=False, download=True, transform=transform
+)
+
+# Filter out labels (originally 0-9), leaving only labels 0 and 1
+idx = np.append(
+    np.where(X_test.targets == 0)[0][:n_samples], np.where(X_test.targets == 1)[0][:n_samples]
+)
+X_test.data = X_test.data[idx]
+X_test.targets = X_test.targets[idx]
+
+# Define torch dataloader with filtered data
+test_loader = DataLoader(X_test, batch_size=batch_size, shuffle=True)
+
+qi = QuantumInstance(Aer.get_backend("aer_simulator_statevector"))
+
+# Define the encoding circuit
+n_qbits = 7
+circuit = QuantumCircuit(n_qbits)
+circuit.h(range(n_qbits))
+
+
+# Populate circuit
+for n in range(n_qbits):
+    for i in range(n_qbits): 
+        circuit.rx(Parameter(str([n,i])),i)
+    if n < n_qbits-1:
+        for j in range(n_qbits-1): 
+            circuit.cx(j,j+1)
+        circuit.barrier(range(n_qbits))
+
+# Define and create QNN
+def create_qnn():
+    feature_map = ZZFeatureMap(2)
+    ansatz = RealAmplitudes(2, reps=1)
+    # REMEMBER TO SET input_gradients=True FOR ENABLING HYBRID GRADIENT BACKPROP
+    qnn = TwoLayerQNN(
+        2,
+        feature_map,
+        ansatz,
+        input_gradients=True,
+        exp_val=AerPauliExpectation(),
+        quantum_instance=qi,
+    )
+    return qnn
+
+qnn = create_qnn()
+print(qnn.operator)
+
+class Net(Module):
+    def __init__(self, qnn):
+        super().__init__()
+        self.conv1 = Conv2d(1, 2, kernel_size=5)
+        self.conv2 = Conv2d(2, 16, kernel_size=5)
+        self.dropout = Dropout2d()
+        self.fc1 = Linear(256, 64)
+        self.fc2 = Linear(64, 2)  # 2-dimensional input to QNN
+        self.qnn = TorchConnector(qnn)  # Apply torch connector, weights chosen
+        # uniformly at random from interval [-1,1].
+        self.fc3 = Linear(1, 1)  # 1-dimensional output from QNN
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        #x = F.relu(self.conv2(x))
+        #x = F.max_pool2d(x, 2)
+        #x = self.dropout(x)
+        x = x.view(x.shape[0], -1)
+        #print(x.shape)
+        #exit()
+        #x = F.relu(self.fc1(x))
+        #x = self.fc2(x)
+        x = self.qnn(x)  # apply QNN
+        x = self.fc3(x)
+        return cat((x, 1 - x), -1)
+
+
+model = Net(qnn)
+
+# Define model, optimizer, and loss function
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+loss_func = NLLLoss()
+
+# Start training
+epochs = 5  # Set number of epochs
+loss_list = []  # Store loss history
+model.train()  # Set model to training mode
+
+for epoch in range(epochs):
+    total_loss = []
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad(set_to_none=True)  # Initialize gradient
+        output = model(data)  # Forward pass
+        loss = loss_func(output, target)  # Calculate loss
+        loss.backward()  # Backward pass
+        optimizer.step()  # Optimize weights
+        total_loss.append(loss.item())  # Store loss
+    loss_list.append(sum(total_loss) / len(total_loss))
+    print("Training [{:.0f}%]\tLoss: {:.4f}".format(100.0 * (epoch + 1) / epochs, loss_list[-1]))
+
+model.eval()  # set model to evaluation mode
+with no_grad():
+
+    correct = 0
+    for batch_idx, (data, target) in enumerate(test_loader):
+        output = model(data)
+        if len(output.shape) == 1:
+            output = output.reshape(1, *output.shape)
+
+        pred = output.argmax(dim=1, keepdim=True)
+        correct += pred.eq(target.view_as(pred)).sum().item()
+
+        loss = loss_func(output, target)
+        total_loss.append(loss.item())
+
+    print(
+        "Performance on test data:\n\tLoss: {:.4f}\n\tAccuracy: {:.1f}%".format(
+            sum(total_loss) / len(total_loss), correct / len(test_loader) / batch_size * 100
+        )
+    )
